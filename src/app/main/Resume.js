863192fc3f46f890a98f657da302592105f7cfd9
rohitsoni8982 <132666867+rohitsoni8982@users.noreply.github.com>
2025-02-03 15:33:12 +0530

    update resume page

--- src/app/main/Resume.js
+++ src/app/main/Resume.js
@@ -49,53 +49,92 @@ const ResumePage = () => {
                                 <h5 className="font-bold">(2019 - 2022)</h5>
                                 <p className="font-bold underline decoration-[#34b7a7]">PMB Gujarati Commerce College, Indore -(DAVV)</p>
                                 <p>
-                                Bachelor of Commerce (B.Com) with Computer Applications is an undergraduate degree program that combines the principles of commerce with the study of computer applications. This course is designed to equip students with knowledge of business practices and the technical skills needed to leverage computer applications in business operations.
+                                    Bachelor of Commerce (B.Com) with Computer Applications is an undergraduate degree program that combines the principles of commerce with the study of computer applications. This course is designed to equip students with knowledge of business practices and the technical skills needed to leverage computer applications in business operations.
                                 </p>
                             </em>
                         </div>
                     </div>
                     <div className="col-lg-6 container mx-auto mb-8" >
-                    <div class="mobile-nav-toggle lg:hidden flex flex-col items-center">
+                        <div class="mobile-nav-toggle lg:hidden flex flex-col items-center">
                             <div class="w-4 h-4 bg-accent border-2 border-[#34b7a7] rounded-full"></div>
                             <div class="h-32 w-[2px] bg-[#34b7a7]"></div>
-                    </div>
-                    <div className="lg:ml-[25%] md:ml-[25%]">
-                        <h3 className="text-3xl font-bold inline-block underline decoration-[#34b7a7] decoration-4 ">Professional Experience</h3>
-                    </div>
+                        </div>
+                        <div className="lg:ml-[25%] md:ml-[25%]">
+                            <h3 className="text-3xl font-bold inline-block underline decoration-[#34b7a7] decoration-4 ">Professional Experience</h3>
+                        </div>
                         <div className="">
                             <ul style={{ listStyleType: "circle" }}>
                                 <li>
-                                <h4 className="font-bold underline decoration-[#34b7a7] decoration-3">Python Development</h4>
+                                    <h4 className="font-bold underline decoration-[#34b7a7] decoration-3">Python Development</h4>
+                                </li>
+                            </ul>
+                            <ul className="pl-4">
+                                <li style={{ listStyleType: "disc" }}>
+                                    <h3 className="font-bold underline decoration-[#34b7a7] decoration-3">FastAPI Development</h3>
+                                </li>
+                                <li>
+                                    <em>Creating Efficient And Optimized API’s</em>
                                 </li>
 
+                                <li style={{ listStyleType: "disc" }}>
+                                    <h3 className="font-bold underline decoration-[#34b7a7] decoration-3">Data Storage & Retrieval</h3>
+                                </li>
+                                <li>
+                                    <em>Handle data operations , such as storing and retrieving information , which was the main part in our application</em>
+                                </li>
+
+                                <li style={{ listStyleType: "disc" }}>
+                                    <h3 className="font-bold underline decoration-[#34b7a7] decoration-3">Text Files Management</h3>
+                                </li>
+                                <li>
+                                    <em>Familiarity with file handling and data storage methods involving text-based
+                                        formats</em>
+                                </li>
+
+                                <li style={{ listStyleType: "disc" }}>
+                                    <h3 className="font-bold underline decoration-[#34b7a7] decoration-3">Python Frame Work (Django)</h3>
+                                </li>
+                                <li>
+                                    <em>Experience With Web Application Development On Python Frame Work
+                                        (Django)</em>
+                                </li>
                             </ul>
-                            <h5>FastAPI Development</h5>
-                            <p>Creating Efficient And Optimized API’s</p>
-                            <h5>Data Storage & Retrieval </h5>
-                            <p>Handle data operations , such as storing and retrieving information , which was the main part in our application</p>
-                            <h5>Text Files Management</h5>
-                            <p>Familiarity with file handling and data storage methods involving text-based
-                            formats</p>
-                            <h5>Python Frame Work (Django)</h5>
-                            <p>Experience With Web Application Development On Python Frame Work
-                            (Django)</p>
                         </div>
+
+                        {/* React */}
                         <div className="">
-                            <h4>React Development</h4>
-                            <ul>
-                                <li>Integrating API’s in React.js application, Facilitating data exchange between
-                                the Front-end and Back-end System.</li>
-                                <li>Present Data in a Visually Appealing and Informative Manner, Organizing and
-                                displaying data in tables, ensuring accuracy and clarity.</li>
-                                <li>Created entities and pages in React Type Script Framework, Created custom
-                                features and functionalities.</li>
-                                <li>Created web forms for developing the application using react type script and HTML controls. Managed deployment on GitHub . Developed a variety of UI elements,such as Card, dropdown list etc.</li>
+                            <ul style={{ listStyleType: "circle" }}>
+                                <li>
+                                    <h4 className="font-bold underline decoration-[#34b7a7] decoration-3">React Development</h4>
+                                </li>
+                            </ul>
+                            <ul className="pl-4">
+                                <li style={{ listStyleType: "disc" }}>
+                                    <em className="">Integrating API’s in React.js application, Facilitating data exchange between the Front-end and Back-end System.</em>
+                                </li>
+                                <li style={{ listStyleType: "disc" }}>
+                                    <em>
+                                        Present Data in a Visually Appealing and Informative Manner, Organizing and
+                                        displaying data in tables, ensuring accuracy and clarity.
+                                    </em>
+                                </li>
+                                <li style={{ listStyleType: "disc" }}>
+                                    <em>
+                                        Created entities and pages in React Type Script Framework, Created custom
+                                        features and functionalities.
+                                    </em>
+                                </li>
+                                <li style={{ listStyleType: "disc" }}>
+                                    <em>
+                                        Created web forms for developing the application using react type script and HTML controls. Managed deployment on GitHub . Developed a variety of UI elements,such as Card, dropdown list etc.
+                                    </em>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <FooterPage/>
+            <FooterPage />
         </div>
 
     )
